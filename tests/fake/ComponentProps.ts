58c8edadcc9247a9f959d7da2db534dd4b284d6c
Thiguet <thiguet2014@gmail.com>
2021-03-13 01:45:17 -0300

    Revert "MGC-2"

--- tests/fake/ComponentProps.ts
+++
@@ -1,9 +0,0 @@
-import faker from 'faker';
-import { SubjectCardProps } from '../../src/components/SubjectCard';
-
-export const getFakeSubjectCardProps = (): SubjectCardProps => ({
-    name: faker.name.title(),
-    hours: faker.random.arrayElement([68, 34]),
-    acronym: faker.random.word().repeat(3),
-    onClick: jest.fn(),
-});
