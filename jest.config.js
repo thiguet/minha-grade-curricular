004138c06d2344d94dbe59eb1c3b391167eeb338
Thiguet <thiguet2014@gmail.com>
2021-03-13 01:36:38 -0300

    chore: updated tests config / aliases

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
     },
     testRegex: '((\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^#/(.*)$': '<rootDir>/tests/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
 };
